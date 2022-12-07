bcf6bc7e287f9c8956df59e7759efa4c18067f96
VitorDevellop <vitor.aguiar@devellop.com.br>
2022-12-06 22:24:11 -0300

    ultimos ajustes de novo

--- Helicoptero.java
+++ Helicoptero.java
@@ -1,3 +1,6 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Helicoptero extends Aeronave {
     private String cor;
     private int capacidade;
@@ -8,10 +11,20 @@ public class Helicoptero extends Aeronave {
         String modelo, 
         String cor,
         int capacidade
-    ){
+    )throws SQLException{
         super(id, marca, modelo);
         this.cor = cor;
         this.capacidade = capacidade;
+
+        PreparedStatement stmt = DAO.createConnection().prepareStatement("INSERT INTO helicoptero (id, marca, modelo, cor, capacidade) VALUES (?, ?, ?, ?, ?)");
+
+        stmt.setInt(1, id);
+        stmt.setString(2, marca);
+        stmt.setString(3, modelo);
+        stmt.setString(4, cor);
+        stmt.setInt(5, capacidade);
+        stmt.execute();
+        stmt.close();
     }
 
     public String getCor() {
@@ -29,6 +42,22 @@ public class Helicoptero extends Aeronave {
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
+
+    public static Helicoptero getByInt(int id) throws SQLException{
+        PreparedStatement stmt = DAO.createConnection().prepareStatement("SELECT * FROM helicoptero WHERE id = ?");
+        stmt.setInt(1, id);
+        stmt.execute();
+        Helicoptero helicoptero = new Helicoptero(
+            stmt.getResultSet().getInt("id"),
+            stmt.getResultSet().getString("marca"),
+            stmt.getResultSet().getString("modelo"),
+            stmt.getResultSet().getString("cor"),
+            stmt.getResultSet().getInt("capacidade")
+        );
+        stmt.close();
+        return helicoptero;
+    }
+
 
     @Override
     public String toString() {
